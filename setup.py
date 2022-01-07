494df211ffaaccb38dd36044c8c1043cbaa37e5a
Fernando Cabrera <fndo.cabrera92@gmail.com>
2022-01-07 20:01:31 -0300

    Se agrego el resto de los elementos

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its a Wafer projects using MLOps to Learn',
+    author='FernandoC',
+    license='MIT',
+)
